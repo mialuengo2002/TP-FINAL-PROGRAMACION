08ead7b9a4aca4cbe0d3717219b0146cfd02d7b9
BelenRodriguez06 <sbarbimaria@gmail.com>
2026-06-30 13:30:27 -0300

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,12 +1,15 @@
-<?php
-$dbserver = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "atr";
+<?php 
+$dbserver = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'atr'; //Modificar con el nombre de la base de datos
 
 $conn = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
 
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+if(!$conn){
+    echo "Houston tenemos un problema";
 }
-?>
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
