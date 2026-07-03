61881c29e7ed6443bfde88013ca09916d71be418
BelenRodriguez06 <sbarbimaria@gmail.com>
2026-07-03 18:00:06 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,35 @@
-<footer>
-<p>Gracias por visitarnos</p>
-</footer>
+<footer class="footer-atr">
+        <section id="contacto" class="container py-5">
+            <h2 class="text-center mb-4">Contacto</h2>
+
+            <div class="text-center">
+                <p>📍 Av. Santa Fe 2450, Recoleta, CABA</p>
+                <p>📞 11 3204-9986</p>
+                <p>✉️ atodoritmo@gmail.com</p>
+            </div>
+
+            <hr>
+
+            <p class="text-center mb-0">
+                © 2026 A Todo Ritmo - Todos los derechos reservados
+            </p>
+        </section>
+
+        <div class="text-center mt-4">
+            <a href="#" class="red-social">
+                <i class="bi bi-instagram"></i>
+            </a>
+
+            <a href="#" class="red-social">
+                <i class="bi bi-facebook"></i>
+            </a>
+
+            <a href="#" class="red-social">
+                <i class="bi bi-youtube"></i>
+            </a>
+
+            <a href="#" class="red-social">
+                <i class="bi bi-tiktok"></i>
+            </a>
+        </div>
+    </footer>
