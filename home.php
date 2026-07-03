64e2316b9a97a613f75f834ece20bdc2c7258040
BelenRodriguez06 <sbarbimaria@gmail.com>
2026-07-03 18:16:18 -0300

    footer

--- home.php
+++ home.php
@@ -14,7 +14,9 @@ if (!isset($_SESSION['id_usuario'])) {
     <link rel="stylesheet" href="home.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="home.css">
+<link rel="stylesheet" href="footer.css">
 </head>
 <body>
 
