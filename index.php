9f82ea502b0cf35f9fa8ea21b5972681dfc9e76e
BelenRodriguez06 <sbarbimaria@gmail.com>
2026-07-03 17:49:17 -0300

    botones

--- index.php
+++ index.php
@@ -38,16 +38,25 @@
                 <a class="navbar-brand d-flex align-items-center" href="#">
                     <img src="img/Mesa de trabajo 5.png" alt="Logo A Todo Ritmo" height="50" class="me-2">
                 </a>
-                <div class="d-flex ms-auto align-items-center">
-                    <a href="ingreso.php" class="btn btn-danger ms-2 iniciar-sesion-btn">Iniciar sesión</a>
-                    <button class="navbar-toggler ms-2" type="button" data-bs-toggle="offcanvas"
-                        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </div>
-                <div class="d-flex ms-auto align-items-center">
-                    <a href="register.php" class="btn btn-danger ms-2 iniciar-sesion-btn">Registrarse</a>
-                </div>
+
+                <div class="d-flex align-items-center ms-auto">
+
+    <a href="ingreso.php" class="btn iniciar-sesion-btn me-2">
+        Iniciar sesión
+    </a>
+
+    <a href="register.php" class="btn iniciar-sesion-btn me-2">
+        Registrarse
+    </a>
+
+    <button class="navbar-toggler" type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasNavbar"
+        aria-controls="offcanvasNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+</div>
                 <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
                     aria-labelledby="offcanvasNavbarLabel">
                     <div class="offcanvas-header">
