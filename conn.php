10f7a7cbeb3e25241945c6d712be7f744f6528c6
BelenRodriguez06 <sbarbimaria@gmail.com>
2026-07-06 13:43:41 -0300

    puerto xampp

--- conn.php
+++ conn.php
@@ -1,15 +1,11 @@
-<?php 
-$dbserver = 'localhost';
-$dbuser = 'root';
-$dbpass = '';
-$dbname = 'atr'; //Modificar con el nombre de la base de datos
-$dbport = 3307;
+<?php
+$dbserver = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "atr";
+$dbport = 3306;
 
 $conn = new mysqli($dbserver, $dbuser, $dbpass, $dbname, $dbport);
-
-if(!$conn){
-    echo "Houston tenemos un problema";
-}
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
