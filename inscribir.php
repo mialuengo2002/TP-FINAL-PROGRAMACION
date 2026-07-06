fc474063cd6a1a14dfa5a1f686f191c7c13be19f
BelenRodriguez06 <sbarbimaria@gmail.com>
2026-07-06 14:33:51 -0300

    Update inscribir.php

--- inscribir.php
+++ inscribir.php
@@ -12,10 +12,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['taller'])) {
     $idTaller = (int) $_POST['taller'];
     $idUsuario = (int) $_SESSION['id_usuario'];
 
-    $sql = "INSERT INTO inscripcion (id_usuario, id_taller) VALUES (?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ii", $idUsuario, $idTaller);
-    $stmt->execute();
+    $sql = "INSERT INTO inscripcion (idAlumno, idTaller) VALUES (?, ?)";
+   $stmt = $conn->prepare($sql);
+$stmt->bind_param("ii", $idUsuario, $idTaller);
+
+if ($stmt->execute()) {
+    echo "Se insertó correctamente.";
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+exit;
+
 
     $stmt->close();
     $conn->close();
